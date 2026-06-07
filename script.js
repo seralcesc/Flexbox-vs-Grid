ebde024c129a768e40e07f41d7a666a97a2130b2
Sergio Alcántara <tesio83@hotmail.com>
2026-06-07 05:34:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Cambia el tema según la preferencia del usuario
 const checkboxInput = document.getElementById("checkbox-theme");
 
 checkboxInput.checked =
@@ -8,3 +9,17 @@ checkboxInput.addEventListener("change", (e) => {
   document.documentElement.setAttribute("data-theme", tema);
   localStorage.setItem("theme", tema);
 });
+
+//menu hamburguesa
+const menuButtonEl = document.querySelector(".menu-button");
+const navMenuEl = document.querySelector(".menu"); // Selecciona tu nav con clase .menu
+
+if (menuButtonEl && navMenuEl) {
+  menuButtonEl.addEventListener("click", () => {
+    // Alternamos la clase unificada en el menú
+    const isNowOpen = navMenuEl.classList.toggle("nav-open");
+
+    // Sincronizamos el estado accesible ARIA
+    menuButtonEl.setAttribute("aria-expanded", isNowOpen);
+  });
+}
